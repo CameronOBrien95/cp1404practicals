10ff93f25b4dadc07d338d66a40b6050156556fa
CameronOBrien95 <cameron.obrien1@my.jcu.edu.au>
2021-03-21 09:43:44 +1000

    Add list exercises program to prac_04 file

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,17 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+numbers = []
+for i in range(0, 5):
+    num = int(input("Number: "))
+    numbers.append(num)
+print("The first number is {}".format(numbers[0]))
+print("The last number is {}".format(numbers[-1]))
+print("The smallest number is {}".format(min(numbers)))
+print("The largest number is {}".format(max(numbers)))
+print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
+
+user_name = input("Please enter your username")
+if user_name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
