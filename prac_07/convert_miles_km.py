e6fb44be7004570bcf5c57e6b88bf9966d42ef67
CameronOBrien95 <cameron.obrien1@my.jcu.edu.au>
2021-04-25 16:06:03 +1000

    Add stage 2 modifications

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+MILE_TO_KM = 1.609344
+
 class ConvertMilesApp(App):
     """ ConvertMilesApp is a Kivy App for converting Miles to Kilometres """
     def build(self):
@@ -11,13 +13,23 @@ class ConvertMilesApp(App):
         return self.root
 
     def handle_calculate(self, value):
-        result = value * 1.609344
+        value = self.get_valid_miles()
+        result = value * MILE_TO_KM
         result = float(result)
         self.root.ids.output_label.text = "{:.3f}".format(result)
 
     def handle_increment(self, value, increment):
-        result = value + increment
-        print(result)
+        if value == '':
+            value = 0
+        result = int(value) + increment
         self.root.ids.input_number.text = str(result)
+        self.handle_calculate(value)
+
+    def get_valid_miles(self):
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0
 
 ConvertMilesApp().run()
