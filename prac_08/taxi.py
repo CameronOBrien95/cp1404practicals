0bb6f1583b085d629cc4d85714401377c66962c3
CameronOBrien95 <cameron.obrien1@my.jcu.edu.au>
2021-05-04 09:45:47 +1000

    Correct issue of copying and pasting the solutions

--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -1,19 +1,17 @@
 """
 CP1404/CP5632 Practical
-Taxi class, derived from Car
+Car class
 """
-
 from prac_08.car import Car
 
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    price_per_km = 1.23
-
-    def __init__(self, name, fuel):
+    def __init__(self, name, fuel, price_per_km):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
+        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -24,9 +22,7 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        # round fare result to nearest 10c
-        fare = round(self.price_per_km * self.current_fare_distance, 1)
-        return fare
+        return self.price_per_km * self.current_fare_distance
 
     def start_fare(self):
         """Begin a new fare."""
