f8efa57e5004f8632ac0a1b2e76b3d3926390d23
CameronOBrien95 <cameron.obrien1@my.jcu.edu.au>
2021-05-08 16:29:30 +1000

    Refactor programs based on feedback provided

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -1,16 +1,20 @@
 from prac_08.taxi import Taxi
 
-class SilverServiceTaxi(Taxi):
 
+class SilverServiceTaxi(Taxi):
+    """Fixed initial surcharge."""
     flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness):
+        """Initialise a Silver Service Taxi instance, based on parent class Taxi."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km += fanciness
 
     def __str__(self):
+        """Return a string with a Taxi and flagfall"""
         return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
 
     def get_fare(self):
+        """Returns price for the Taxi trip"""
         return self.flagfall + super().get_fare()
--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -2,23 +2,23 @@ from prac_08.car import Car
 from prac_08.silver_service_taxi import SilverServiceTaxi
 from prac_08.taxi import Taxi
 
-MENU = "1) choose taxi\n2) Drive\n3) Bill\n4) Quit"
+MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
     taxis = [Taxi("Prius", 100), Taxi("Kia", 150), SilverServiceTaxi("Hummer", 200, 4)
-        ,SilverServiceTaxi("Limo", 250, 6)]
+        , SilverServiceTaxi("Limo", 250, 6)]
     total_bill = 0
-    choice = int(input("{}\n>>>".format(MENU)))
-    current_taxi = "none"
-    while choice != 4:
-        if choice == 1:
+    choice = input("{}\n>>>".format(MENU)).lower()
+    current_taxi = None
+    while choice != "q":
+        if choice == "c":
             display_taxis(taxis)
             taxi_choice = int(input("Please choose a taxi"))
             current_taxi = taxis[taxi_choice]
             print("{} Chosen".format(taxis[taxi_choice]))
-        elif choice == 2:
-            if current_taxi == "none":
+        elif choice == "d":
+            if current_taxi is None:
                 print("You need to pick a taxi first")
             else:
                 current_taxi.start_fare()
@@ -27,12 +27,13 @@ def main():
                 fare_cost = current_taxi.get_fare()
                 total_bill += fare_cost
                 print("your trip cost you ${:.2f}".format(fare_cost))
-        elif choice == 3:
-            print("Your total bill is ${:.2f}".format(total_bill))
-        choice = int(input("{}\n>>>".format(MENU)))
+        print("Your total bill is ${:.2f}".format(total_bill))
+        choice = input("{}\n>>>".format(MENU)).lower()
 
 
 def display_taxis(taxis):
     for i, taxi in enumerate(taxis):
-        print("{} - {}".format(i+1, taxi))
+        print("{} - {}".format(i + 1, taxi))
+
+
 main()
